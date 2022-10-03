11bdcfeb51815a7596687e3f55f032ac41d09d12
KeeveRW11 <keevewhyte+github@gmail.com>
2022-10-03 15:12:27 -0400

    server live

--- server.js
+++ server.js
@@ -18,10 +18,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(require('./controllers/'));
 
-/*sequelize.sync({ force: false }).then(() => {
+/*sequelize.sync({ force: true }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
-*/
+});*/
+
 
 app.listen(PORT, () => {
     console.log(`App listening on port ${PORT}!`);
