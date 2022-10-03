c31205f9e5b712d7aab36367cf51a6f65663fe6e
KeeveRW11 <keevewhyte+github@gmail.com>
2022-10-03 16:37:59 -0400

    user route set

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-/*
+
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
 const dashboardRoutes = require('./dashboard-routes.js');
@@ -7,6 +7,6 @@ const dashboardRoutes = require('./dashboard-routes.js');
 router.use('/', homeRoutes);
 router.use('/dashboard', dashboardRoutes);
 router.use('/api', apiRoutes);
-*/
+
 
 module.exports = router;
