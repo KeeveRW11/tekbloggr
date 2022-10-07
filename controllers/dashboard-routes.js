c94483a36f3a699f937e2a5279e4d00d29f158fd
KeeveRW11 <keevewhyte+github@gmail.com>
2022-10-06 22:18:29 -0400

    routes push

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -15,20 +15,20 @@ router.get('/', withAuth, (req, res) => {
             'post_text',
             'title',
             'created_at',
-        //   ],
-        // include: [
-        //     {
-        //         model: Comment,
-        //         attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
-        //         include: {
-        //             model: User,
-        //             attributes: ['username']
-        //         }
-        //     },
-        //     {
-        //         model: User,
-        //         attributes: ['username']
-        //     }
+          ],
+        include: [
+            {
+                model: Comment,
+                attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include: {
+                    model: User,
+                    attributes: ['username']
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
         ]
     })
     .then(dbPostData => {
